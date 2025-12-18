6c142185fda7d813a96aa878dafb13e74c1a970b
kodega2016 <khadgalovecoding2016@gmail.com>
2025-12-18 22:13:16 +0545

    feat:added test cases

--- internal/forms/forms_test.go
+++ internal/forms/forms_test.go
@@ -42,3 +42,45 @@ func TestForm_Required(t *testing.T) {
 	}
 
 }
+
+func TestForm_MinLength(t *testing.T) {
+	r := httptest.NewRequest("POST", "/random", nil)
+	data := url.Values{}
+	data.Add("name", "ku")
+	r.PostForm = data
+
+	form := New(r.PostForm)
+	form.MinLength("name", 3, r)
+	isValid := form.Valid()
+
+	if isValid {
+		t.Error("got form valid when minlenght is not valid")
+	}
+
+	data = url.Values{}
+	data.Add("name", "khadga shrestha")
+	r.PostForm = data
+
+	form = New(r.PostForm)
+	isValid = form.Valid()
+
+	if !isValid {
+		t.Error("got form invalid when it should be valid")
+	}
+
+}
+
+func TestForm_Has(t *testing.T) {
+	r := httptest.NewRequest("POST", "/random", nil)
+	data := url.Values{}
+	r.PostForm = data
+
+	form := New(r.PostForm)
+	form.Has("name")
+	isValid := form.Valid()
+
+	if isValid {
+		t.Error("got form valid when name has no value")
+	}
+
+}
