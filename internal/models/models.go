5f220ea61908df99334f5ec48380c75187e4306c
kodega2016 <khadgalovecoding2016@gmail.com>
2025-12-24 09:51:35 +0545

    feat:added other model properties

--- internal/models/models.go
+++ internal/models/models.go
@@ -3,11 +3,17 @@ package models
 import "time"
 
 type Reservation struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-	Phone     string `json:"phone"`
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	Phone     string    `json:"phone"`
+	StartDate time.Time `json:"start_date"`
+	EndDate   time.Time `json:"end_date"`
+	CreatedAt time.Time `json:"created_at"`
+	RoomID    int       `json:"room_id"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Room      Room
 }
 
 type User struct {
@@ -15,9 +21,10 @@ type User struct {
 	FirstName   string    `json:"first_name"`
 	LastName    string    `json:"last_name"`
 	Email       string    `json:"email"`
+	Password    string    `json:"password"`
+	AccessLevel int       `json:"access_level"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
-	AccessLevel int       `json:"access_level"`
 }
 
 type Room struct {
@@ -28,6 +35,23 @@ type Room struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
+type Restriction struct {
+	ID              int       `json:"id"`
+	RestrictionName string    `json:"restriction_name"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+}
+
 type RoomRestriction struct {
-	ID int `json:"id"`
+	ID            int       `json:"id"`
+	StartDate     time.Time `json:"start_date"`
+	EndDate       time.Time `json:"end_date"`
+	RoomID        int       `json:"room_id"`
+	ReservationID int       `json:"reservation_id"`
+	RestrictionID int       `json:"restriction_id"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+	Room          Room
+	Reservation   Reservation
+	Restriction   Reservation
 }
